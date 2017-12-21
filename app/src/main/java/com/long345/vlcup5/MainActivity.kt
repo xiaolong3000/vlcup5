1823ef910136d8e7cb938396707615cbeb2963fc
xiaolong3000 <276947850@qq.com>
2017-12-21 23:04:17 +0800

    小的修改

--- app/src/main/java/com/long345/vlcup5/MainActivity.kt
+++ app/src/main/java/com/long345/vlcup5/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.long345.vlcup5
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
@@ -20,8 +21,10 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 import android.content.DialogInterface
+import android.graphics.drawable.Drawable
 import android.net.wifi.WifiManager
-
+import android.text.Editable
+import android.text.TextWatcher
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,20 +33,19 @@ class MainActivity : AppCompatActivity() {
     var filepath = ""
 
 
-
-
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-        var wifi= applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        if (wifi.disconnect()){
-            var builder=AlertDialog.Builder(this@MainActivity)
-            builder.setTitle("").setMessage("wifi没有连接").setPositiveButton("OK",DialogInterface.OnClickListener(
-                    fun(dia:DialogInterface,num:Int) {
-                this@MainActivity.finish()
+        val wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if (wifi.disconnect()) {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("").setMessage("wifi没有连接").setPositiveButton("OK", DialogInterface.OnClickListener(
+                    fun(dia: DialogInterface, num: Int) {
+                        this@MainActivity.finish()
 
-            }))
+                    }))
         }
         setContentView(R.layout.activity_main)
 
@@ -53,20 +55,23 @@ class MainActivity : AppCompatActivity() {
 
 
         button2.setOnClickListener {
-//            val intent = Intent()
+            //            val intent = Intent()
 //            intent.type = "image/*"
 //            intent.action = Intent.ACTION_GET_CONTENT
 //            startActivityForResult(intent, 1)
-            val builder=AlertDialog.Builder(this@MainActivity)
-            val path="" + Environment.getExternalStorageDirectory() + "/DCIM/Camera"
-            val allfile=File(path)
-            val list=allfile.list()
-        //    list.filter { it.contains(".") }
-            builder.setTitle("选择上传文件").setItems(list,DialogInterface.OnClickListener(
-                    fun (_,num:Int){
-                       // println(allfile.list().get(num))
+            val builder = AlertDialog.Builder(this@MainActivity)
+            val path = "" + Environment.getExternalStorageDirectory() + "/DCIM/Camera"
+            val allfile = File(path)
+            val list = allfile.list()
 
-                        filename=allfile.list().get(num)
+
+            //    list.filter { it.contains(".") }
+
+            builder.setTitle("选择上传文件").setItems(list, DialogInterface.OnClickListener(
+                    fun(_, num: Int) {
+                        // println(allfile.list().get(num))
+
+                        filename = allfile.list().get(num)
                         filepath = "" + Environment.getExternalStorageDirectory() + "/DCIM/Camera"
                     }
             )).show()
@@ -82,19 +87,19 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-wenhao.setOnClickListener {
-    var builder=AlertDialog.Builder(this@MainActivity)
-    builder.setTitle("").setMessage("点击上传没有反应，无任何提示，进度条不动。\r\n第一请检查视频是否存储在SD卡上!\r\n第二请检查是否选错了上传部门").show()
-}
+        wenhao.setOnClickListener {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("").setMessage("点击上传没有反应，无任何提示，进度条不动。\r\n第一请检查视频是否存储在SD卡上!\r\n第二请检查是否选错了上传部门").show()
+        }
 
-        imageButton.setOnClickListener{
+        imageButton.setOnClickListener {
 
-           var builder=AlertDialog.Builder(this@MainActivity)
-           builder.setTitle("").setItems(chaege.bumens, DialogInterface.OnClickListener(
-                    fun(d:DialogInterface, num:Int) {
-                       var share=getSharedPreferences("bumen",Context.MODE_PRIVATE)
-                        share.edit().putString("bumen",chaege.bumens[num]).apply()
-                        bumen.text=chaege.bumens[num]
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("").setItems(chaege.bumens, DialogInterface.OnClickListener(
+                    fun(d: DialogInterface, num: Int) {
+                        val share = getSharedPreferences("bumen", Context.MODE_PRIVATE)
+                        share.edit().putString("bumen", chaege.bumens[num]).apply()
+                        bumen.text = chaege.bumens[num]
 
                     })
             )
@@ -103,67 +108,67 @@ wenhao.setOnClickListener {
 
         }
         textView4.text = SimpleDateFormat("yyyy-MM-dd").format(Date())
-        var share=getSharedPreferences("bumen", Context.MODE_PRIVATE)
-        bumen.text=share.getString("bumen","未设置")
+        val share = getSharedPreferences("bumen", Context.MODE_PRIVATE)
+        bumen.text = share.getString("bumen", "未设置")
 
         imageButton5.setOnClickListener {
-            val builder=AlertDialog.Builder(this@MainActivity)
-            builder.setTitle("").setItems(chaege.renyuans,DialogInterface.OnClickListener(
-                    fun (d:DialogInterface,num:Int){
-                        val share=getSharedPreferences("renyuan",Context.MODE_PRIVATE)
-                        share.edit().putString("renyuan",chaege.renyuans[num]).apply()
-                        renyuan.text=chaege.renyuans[num]
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("").setItems(chaege.renyuans, DialogInterface.OnClickListener(
+                    fun(d: DialogInterface, num: Int) {
+                        val share = getSharedPreferences("renyuan", Context.MODE_PRIVATE)
+                        share.edit().putString("renyuan", chaege.renyuans[num]).apply()
+                        renyuan.text = chaege.renyuans[num]
                     }
             ))
             builder.show()
         }
-        var share2=getSharedPreferences("renyuan",Context.MODE_PRIVATE)
-        renyuan.text=share2.getString("renyuan","未设置")
+        var share2 = getSharedPreferences("renyuan", Context.MODE_PRIVATE)
+        renyuan.text = share2.getString("renyuan", "未设置")
 
 
     }
 
     var runnable = Runnable {
         kotlin.run {
-          if (!bumen.equals("未设置")&&!renyuan.equals("未设置")) {
+            if (!bumen.equals("未设置") && !renyuan.equals("未设置")) {
 
 
-              if(filename!="") {
-                  Looper.prepare()
-                  val ftp = f1(chaege.c(bumen.text.toString()), 21, "ls", "ls")
-                  val login= ftp.ftpLogin()
-                  if (login) {
-                      val localfile=File(filepath + "/" + filename)
-                         if(localfile.exists()){
-                       val   success = ftp.uploadFile(localfile, renyuan.text.toString(), progressBar)
-                         if (success) {
+                if (filename != "") {
+                    Looper.prepare()
+                    val ftp = f1(chaege.c(bumen.text.toString()), 21, "ls", "ls")
+                    val login = ftp.ftpLogin()
+                    if (login) {
+                        val localfile = File(filepath + "/" + filename)
+                        if (localfile.exists()) {
+                            val success = ftp.uploadFile(localfile, renyuan.text.toString(), progressBar)
+                            if (success) {
 
-                             filename=""//重置
-                             Toast.makeText(this@MainActivity, "上传成功", Toast.LENGTH_LONG).show()
+                                filename = ""//重置
+                                Toast.makeText(this@MainActivity, "上传成功", Toast.LENGTH_LONG).show()
 
-                         }
-                     }else{
-                         Toast.makeText(this@MainActivity, "选择的文件已经被删除", Toast.LENGTH_LONG).show()
-                     }
+                            }
+                        } else {
+                            Toast.makeText(this@MainActivity, "选择的文件已经被删除", Toast.LENGTH_LONG).show()
+                        }
 
 
-                      ftp.ftpLogOut()
+                        ftp.ftpLogOut()
 
-                  }else{
-                      Toast.makeText(this@MainActivity, "连接服务器失败", Toast.LENGTH_LONG).show()
-                  }
-                  Looper.loop()
+                    } else {
+                        Toast.makeText(this@MainActivity, "连接服务器失败", Toast.LENGTH_LONG).show()
+                    }
+                    Looper.loop()
 
-              }else{
-                  Looper.prepare()
-                  Toast.makeText(this@MainActivity,"选择文件为空,请选择文件",Toast.LENGTH_LONG).show()
-                  Looper.loop()
-              }
-           }else{
-              Looper.prepare()
-              Toast.makeText(this@MainActivity,"部门及人员均要设置",Toast.LENGTH_LONG).show()
-              Looper.loop()
-          }
+                } else {
+                    Looper.prepare()
+                    Toast.makeText(this@MainActivity, "选择文件为空,请选择文件", Toast.LENGTH_LONG).show()
+                    Looper.loop()
+                }
+            } else {
+                Looper.prepare()
+                Toast.makeText(this@MainActivity, "部门及人员均要设置", Toast.LENGTH_LONG).show()
+                Looper.loop()
+            }
 //            Looper.prepare()
 //            if (ftplogin("192.168.31.190","ls","ls")){
 //                Toast.makeText(this@MainActivity, "连接成功", Toast.LENGTH_LONG).show()
@@ -180,86 +185,11 @@ wenhao.setOnClickListener {
 
         }
     }
-
-
-
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode == 1) {
-//            if (resultCode == Activity.RESULT_OK) {
-//                var uri = data?.data
-//                var cosur = contentResolver.query(uri, null, null, null, null)
-//                cosur.moveToFirst()
-//                var v_name = cosur.getString(2)
-//
-//                filename = v_name
-//
-//
-//
-//            }
-//        }
-//    }
-
-//    fun ftplogin(ip: String, ftpuser: String, ftppass: String): Boolean {
-//        var isLogin = false
-//        var ftpconfig = FTPClientConfig()
-//
-//        ftpconfig.serverTimeZoneId = TimeZone.getDefault().id
-//        ftpclient.controlEncoding = "utf-8"
-//        ftpclient.configure(ftpconfig)
-//
-//        ftpclient.connect(ip,21)
-//        var reply = ftpclient.replyCode
-//        if (!FTPReply.isPositiveCompletion(reply)) {
-//            ftpclient.disconnect()
-//        //    print("??????????????????????????")
-//            return isLogin
-//        }
-//
-//       ftpclient.login(ftpuser, ftppass)
-//
-//        ftpclient.enterLocalPassiveMode()
-//        ftpclient.setFileType(FTPClient.BINARY_FILE_TYPE)
-//        isLogin = true
-//
-//
-//        return isLogin
-//    }
-
-//    fun ftplogout() {
-//        try {
-//            if (null != ftpclient && ftpclient.isConnected) {
-//                ftpclient.logout()
-//              //  Toast.makeText(this@MainActivity,"断开连接",Toast.LENGTH_SHORT)
-//            }
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        } finally {
-//            ftpclient.disconnect()
-//        }
-//
-//    }
-
-//    fun uploadfile(localfile: File, remotepath: String) {
-//
-//        var startTime = System.currentTimeMillis()
-//        var instream = BufferedInputStream(FileInputStream(localfile))
-//        try {
-//
-//
-//            ftpclient.changeWorkingDirectory(remotepath)
-//            ftpclient.copyStreamListener = FTPProcess(this@MainActivity,startTime)
-//             ftpclient.storeFile(localfile.name, instream)
-//
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        } finally {
-//            instream.close()
-//        }
-//
-//    }
 }
+
+
+
+
 
 
 
