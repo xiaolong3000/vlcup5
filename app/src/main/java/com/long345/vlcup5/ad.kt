d44820ef84a67c8f11f526bdbdb92c1be4406b8d
xiaolong3000 <276947850@qq.com>
2018-04-28 10:26:56 +0800

    自动更新改为手动更新

--- app/src/main/java/com/long345/vlcup5/ad.kt
+++ app/src/main/java/com/long345/vlcup5/ad.kt
@@ -12,6 +12,7 @@ import android.os.Environment
 import android.os.Handler
 import android.widget.Switch
 import com.long345.vlcup5.chaege.mainip
+import kotlinx.android.synthetic.main.activity_ad.*
 import java.io.File
 import java.net.URL
 import java.util.*
@@ -28,46 +29,78 @@ class ad : AppCompatActivity() {
                 val ftp = f1(mainip, 21, "ls", "ls")
                 val login = ftp.ftpLogin()
                 if (login){
+
+
+
               //  val versiontxt = URL("ftp://ls:ls@${chaege.mainip}/version.txt").readText()//http://192.168.31.190:8080/vlc/version.xml
                 val su=ftp.downfile("version.txt",filepath+"/version.txt")
                 val versiontxt=File(filepath+"/version.txt").readText()
-                    val bumen=chaege.nowbanben
+                val bumen=chaege.nowbanben
                 val regex = Regex("<$bumen>([0-9]+)<$bumen>")
                 val result = regex.find(versiontxt)!!.value.replace("<$bumen>", "")
                 val version = result.toInt()
-                val share_version = getSharedPreferences("myversion", Context.MODE_PRIVATE)
-                val thisversion = share_version.getString("myversion", "0").toInt()
-                if (thisversion == 0) {
-                    share_version.edit().putString("myversion", "" + version).apply()
-                    val intent=Intent(this@ad,MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else if (version > thisversion) {
-                   // println("here")
+                    val mytxt=File(filepath+"/myversion.txt")
+                    if (!mytxt.exists()){
+                        mytxt.mkdir()
+                       mytxt.writeText("0")
+                    }
+
+//                val share_version = getSharedPreferences("myversion", Context.MODE_PRIVATE)
+//                val thisversion = share_version.getString("myversion", "0").toInt()
+                val thisversion=mytxt.readText().toInt()
+                    textview2.text="服务器ip:$mainip,版本号:$version,本机版本号:$thisversion"
+
+
+
+                    button2.setOnClickListener {
                         val success=ftp.downfile("$bumen.apk", "$filepath/$bumen.apk")
                         if (success){
                             val intent=Intent(Intent.ACTION_VIEW)
                             intent.setDataAndType(Uri.fromFile(File("$filepath/$bumen.apk")),"application/vnd.android.package-archive")
-                            share_version.edit().putString("myversion",version.toString()).apply()
+                            mytxt.writeText("$version")
                             startActivity(intent)
                         }
-                    }else{
-                    println("正常启动")
-                    val timer=Timer()
-                   timer.schedule(object :TimerTask(){
-                       override fun run() {
+                    }
+                    button3.setOnClickListener {
                            val intent=Intent(this@ad,MainActivity::class.java)
                            startActivity(intent)
                            finish()
-                       }
-                   },1000)
                     }
+
+
+//                if (thisversion == 0) {
+//                   // share_version.edit().putString("myversion", "" + version).apply()
+//                    val intent=Intent(this@ad,MainActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+//                } else if (version > thisversion) {
+//                   // println("here")
+//                        val success=ftp.downfile("$bumen.apk", "$filepath/$bumen.apk")
+//                        if (success){
+//                            val intent=Intent(Intent.ACTION_VIEW)
+//                            intent.setDataAndType(Uri.fromFile(File("$filepath/$bumen.apk")),"application/vnd.android.package-archive")
+//                            share_version.edit().putString("myversion",version.toString()).apply()
+//                            startActivity(intent)
+//                        }
+//                    }else{
+////                    println("正常启动")
+////                    val timer=Timer()
+////                   timer.schedule(object :TimerTask(){
+////                       override fun run() {
+////                           val intent=Intent(this@ad,MainActivity::class.java)
+////                           startActivity(intent)
+////                           finish()
+////                       }
+////                   },1000)
+//                    }
                   }else{//网络没有的话立即跳转
 
-                            val intent=Intent(this@ad,MainActivity::class.java)
-                            startActivity(intent)
-                            finish()
+//                            val intent=Intent(this@ad,MainActivity::class.java)
+//                            startActivity(intent)
+//                            finish()
+                    textview2.text="网络无法联通，请检查网络"
                 }
+
 
                 ftp.ftpLogOut()
 
