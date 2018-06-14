13b63669c9272576825fecd262608792edd72909
xiaolong3000 <276947850@qq.com>
2018-06-14 17:38:54 +0800

    商检

--- app/src/main/java/com/long345/vlcup5/chaege.java
+++ app/src/main/java/com/long345/vlcup5/chaege.java
@@ -14,7 +14,7 @@ public class chaege {
 
     }
 
-    public static String mainip="192.168.1.40";
+    public static String mainip="192.168.1.40";//服务器地址，需要更改
     public static String [] bumens={"上北外勤","上南外勤","下北外勤","下南外勤","客场外勤"};
     public static String [] bumens2={"南商检","北商检"};
     public static String [] bumens3={"商检","北外勤","南外勤"};
@@ -23,8 +23,8 @@ public class chaege {
 
 
 
-    public  static final String nowbanben="waiqin";//需要更改
-    public static final String[] thebumen=bumens;//需要更改
+    public  static final String nowbanben="shangjian";//需要更改
+    public static final String[] thebumen=bumens2;//需要更改
 
 
 
